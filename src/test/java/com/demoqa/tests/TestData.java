50af59e16db4ee0532db0e2fa8f6a8de6d44e09f
Yura Kondratiev <yuraa.kondratiev@yandex.ru>
2023-05-24 21:23:45 +0300

    added RandomUtils

---
+++ src/test/java/com/demoqa/tests/TestData.java
@@ -0,0 +1,7 @@
+package com.demoqa.tests;
+
+public class TestData {
+  public static   String firsName = "Ivan",
+                        lastName = "Petrov",
+                        userEmail = "ivanpetrov@mail.com";
+}
