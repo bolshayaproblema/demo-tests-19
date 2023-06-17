3ccc00154daf00b37a90570e04529dd2b0e7dd19
Yura Kondratiev <yuraa.kondratiev@yandex.ru>
2023-06-17 10:32:04 +0300

    added remote tests with selenoid

--- src/test/java/com/demoqa/tests/RemoteTestBase.java
+++ src/test/java/com/demoqa/tests/RemoteTestBase.java
@@ -3,6 +3,9 @@ package com.demoqa.tests;
 import com.codeborne.selenide.Configuration;
 import com.demoqa.pages.RegistrationPage;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class RemoteTestBase {
     RegistrationPage registrationPage = new  RegistrationPage();
@@ -13,5 +16,15 @@ public class RemoteTestBase {
         Configuration.browserSize = "1900 × 1000";
         Configuration.holdBrowserOpen = true;
         Configuration.pageLoadStrategy = "eager";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
     }
 }
