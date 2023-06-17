d0bf6494a9a8497037929bf315e241cda18b08db
Yura Kondratiev <yuraa.kondratiev@yandex.ru>
2023-06-17 11:27:23 +0300

    added attaches(screen, page source, logs, video)

--- src/test/java/com/demoqa/tests/RemoteTestBase.java
+++ src/test/java/com/demoqa/tests/RemoteTestBase.java
@@ -45,6 +45,6 @@ public class RemoteTestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        // video
+        Attach.addVideo();
     }
 }
