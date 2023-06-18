79fb81ebbdab0bab9f8ad0d68a893c2a63cf84d4
Yura Kondratiev <yuraa.kondratiev@yandex.ru>
2023-06-18 15:54:07 +0300

    added Configuration

--- src/test/java/com/demoqa/tests/RemoteTestBase.java
+++ src/test/java/com/demoqa/tests/RemoteTestBase.java
@@ -17,12 +17,13 @@ public class RemoteTestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1900 × 1000";
         Configuration.holdBrowserOpen = true;
         Configuration.pageLoadStrategy = "eager";
-
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
--- src/test/java/com/demoqa/tests/RemoteTests.java
+++ src/test/java/com/demoqa/tests/RemoteTests.java
@@ -1,9 +1,7 @@
 package com.demoqa.tests;
 
 
-import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
-import io.qameta.allure.Step;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
